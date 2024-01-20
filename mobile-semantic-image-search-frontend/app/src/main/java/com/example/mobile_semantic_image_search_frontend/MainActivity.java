84d0a8ed109df47aa26d666b533445913df7f246
hdkhang6803 <hdkhang6803@gmail.com>
2024-01-20 22:38:45 +0700

    init

---
+++ mobile-semantic-image-search-frontend/app/src/main/java/com/example/mobile_semantic_image_search_frontend/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.mobile_semantic_image_search_frontend;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ mobile-semantic-image-search-frontend/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "mobile-semantic-image-search-frontend"
+include(":app")
+ 
