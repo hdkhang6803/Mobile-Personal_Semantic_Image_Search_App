01f2c665a9216f424d43346efc569fcebf81bb1f
Hoang Do <hoang2003ngolike@gmail.com>
2024-01-21 23:03:11 +0700

    batch upload working

--- mobile-semantic-image-search-frontend/app/src/main/java/com/example/mobile_semantic_image_search_frontend/MainActivity.java
+++ mobile-semantic-image-search-frontend/app/src/main/java/com/example/mobile_semantic_image_search_frontend/MainActivity.java
@@ -1,8 +1,25 @@
 package com.example.mobile_semantic_image_search_frontend;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.content.ContentUris;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
+
+import android.Manifest;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +27,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button startServiceButton = findViewById(R.id.startServiceButton);
+        startServiceButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startBackgroundService();
+            }
+
+        });
+
+    }
+
+    private void startBackgroundService() {
+        Intent serviceIntent = new Intent(this, BackgroundService.class);
+        startService(serviceIntent);
     }
 }
