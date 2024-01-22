f6f01e95900984c73d7c08a8bb8adc7d9db0860a
minhquanBr2 <minhquanhole03@gmail.com>
2024-01-22 09:37:11 +0700

    fix import

--- mobile-semantic-image-search-backend/routes/query_route.py
+++ mobile-semantic-image-search-backend/routes/query_route.py
@@ -8,7 +8,7 @@ import pandas as pd
 from io import BytesIO
 import base64
 import matplotlib.pyplot as plt
-from data.index_cache_helper import load_index
+from ..data.index_cache_helper import load_index
 
 
 def txt_query_search_route(app, model, index_cache, userIds):
